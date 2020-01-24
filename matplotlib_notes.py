9b29b246170c05b23ae9a4253c5556a03b56aeac
dragosbratuflorian19 <dragosbratuflorian19@gmail.com>
2020-01-24 17:17:24 +0200

    Matplotlib notes

---
+++ matplotlib_notes.py
@@ -0,0 +1,29 @@
+##################################################################################
+# import matplotlib/fit a graph/show it
+import matplotlib.pyplot as plt
+plt.plot([1,2,3], [4,5,6])
+plt.show()
+##################################################################################
+# Labels/Legends/titles
+x = [1, 2, 3]
+y = [4, 2, 1]
+x2 = [1, 2, 3]
+y2 = [14, 12, 11]
+
+plt.plot(x1, y2, label='First line')
+plt.plot(x2, y2, label='Second line')
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.title('Graph title\nSubtitle')
+plt.legend()
+plt.show()
+##################################################################################
+# Barcharts
+x = [2 , 4, 6, 8, 10]
+y  = [6,7,8,2,4]
+plt.bar(x, y, label='Bars1', color='r')
+##################################################################################
+# Histograms
+git config --global user.email 'dragosbratuflorian19@gmail.com'
+git config --global user.name 'dragosbratuflorian19'
+git add . != git reset

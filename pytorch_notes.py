aab0a45e9e71e65dde93a5c4626abd18a0ecd3f0
dragosbratuflorian19 <dragosbratuflorian19@gmail.com>
2020-02-29 13:17:33 +0200

    sda

--- pytorch_notes.py
+++ pytorch_notes.py
@@ -258,6 +258,118 @@ t.mean(dim=0) # tensor([2.6, 2., 1.3, 3.])
 t.mean(dim=0).tolist() # [2.6, 2., 1.3, 3.]
 t.mean(dim=0).numpy() # array([2.6, 2., 1.3, 3.], dtype=float32) 
 ###########################################################################
+# MNIST data set transformations:
+# PNG
+# trimming
+# resizing
+# sharpening
+# extending
+# negating
+# grayscaling
+###########################################################################
+# 4 steps of AI implementation
+# Prepare the data : ETL process (Extract; Transform ; Load)
+# Build the model
+# Train the model
+# Analyze the model's results
+###########################################################################
+class OHLC(Dataset):
+	def __init__(self, csv_file):
+		self.data = pd.read_csv(csv_file)
+
+	def __getitem__(self, index):
+		r = self.data.iloc[index]:
+		label = torch.tensor(r.is_up_day, dtype=torch.long)
+		sample = self.normalize(torch.tensor([r.open, r.high, r.low, r.close]))
+		return sample, label
+
+	def __len__(self):
+		return len(self.data)
+###########################################################################
+# Loading the data
+import torch # top level pytorch package
+import torchvision # acces to popular datasets and image transformation
+import torchvision.transforms as transforms # gives acces to common transformation to image processing
+
+train_set = torchvision.datasets.FashionMNIST(
+	root='./data/FashionMNIST', # the location on disk where the data is located
+	train=True, # the train set
+	download=True, # tells the class to download the data if it's not present at the location we precised
+	transforms=transforms.Compose([ # Compose class allows us to do more than 1 transformations
+		transforms.ToTensor() # we need tensors
+	])
+)
+
+train_loader = torch.utils.data.Dataloader(train_set, batch_size=10) # we can shuffle, have batch size ( quarrying )
+###########################################################################
+# Visualize the data
+import numpy as np
+import matplotlib.pyplot as plt
+
+torch.set_printoptions(linewidth=120)
+train_set.train_labels # tensor([9, 0, 0 ... 5])
+train_set.train_labels.bincount() # tensor([6000, 6000, ...., 6000]) the frequency of the data
+
+# One sample
+sample = next(iter(train_set))
+len(sample) # 2
+type(sample) # tuple
+image, label = sample
+image.shape # torch.Size([1, 28, 28])
+label.shape # torch.Size([]) ; scalar
+plt.imshow(image.squeeze(), cmap='gray')
+
+# One batch
+batch = next(iter(train_loader))
+len(batch) # 2
+type(batch) # list
+images, labels = batch
+images.shape # torch.Size([10, 1, 28, 28])
+label.shape # torch.Size([10]) # rank one tensor
+grid = torchvision.utils.make_grid(images, nrows=10)
+
+plt.figure(figsize=(15, 15))
+plt.imshow(np.transpose(grid, (1, 2, 0)))
+###########################################################################
+# Build the model: with torch.nn
+import torch.nn as nn
+
+class Network(nn.Module):
+	def __init__(self):
+		super(Network, self).__init()
+		self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5)
+		self.conv2 = nn.Conv2d(in_channels=6, out_channels=12, kernel_size=5)
+
+		self.fc1 = nn.Linear(in_features=12*4*4, out_features=120) # fully connected / Dense / Linear layers
+		self.fc2 = nn.Linear(in_features=120, out_features=60)
+		self.out = nn.Linear(in_features=60, out_features=10)
+
+
+	def forward(self, t):
+		t = self.layer(t)
+		return t
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
